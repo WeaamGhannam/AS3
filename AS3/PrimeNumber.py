30af2de5165b76e121bcf4f610746ea370ccfea0
ENG.WEAAM <91812443+WeaamGhannam@users.noreply.github.com>
2025-05-09 09:42:25 +0300

    Add files via upload
    Question 1: Word Counter
    Ask the user for a file name.
    Count and display the number of words in the file.
    Also, identify the most frequent word and how many times it appears.
    
    Question 2: Prime Number Analyzer
    Let the user input a range of numbers.
    Use a function to check if a number is prime.

---
+++ AS3/PrimeNumber.py
@@ -0,0 +1,29 @@
+with open("primenumder.txt","r") as file:
+      file.read()
+#Use a function to check if a number is prime.
+def is_prime(num):
+  if num > 1:
+    for i in range(2,num):
+      if (num % i) == 0:
+        return False
+    return True
+  else:
+    return False
+
+# Let the user input a range of numbers.
+
+min_limit = int(input("Enter the min limit of the range: "))
+max_limit = int(input("Enter the max limit of the range: "))
+# error checking 
+if max_limit <= min_limit:
+  print("The max limit should be greater than the min limit")
+
+# printing primes 
+
+print(f"Numbers that are prime in the range ({min_limit},{max_limit}) are:")
+for number in range(min_limit,max_limit+1):
+  if is_prime(number):
+    print(number,end=" ")
+  
+
+file.close()
